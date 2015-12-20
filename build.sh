df27f27f8b1ee5a018a224fc4a33379b95a593ee
Rene Richter <Richterrettich@gmail.com>
2015-12-20 15:14:39 +0100

    Progress

--- build.sh
+++ build.sh
@@ -6,8 +6,8 @@ go install
 cd ..
 go test
 GO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o out/main .
-docker build -t openservice/acl-service:latest .
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_REPO_SLUG}" = "InteractiveLecture/acl-service" ] ; then
+docker build -t openservice/media-service:latest .
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_REPO_SLUG}" = "InteractiveLecture/media-service" ] ; then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" -e="$DOCKER_EMAIL"
-  docker push openservice/acl-service:latest
+  docker push openservice/media-service:latest
 fi
