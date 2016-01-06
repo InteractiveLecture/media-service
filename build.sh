aa93f5ed62d4aaeb04e8007ff4f47ea27abf8948
Rene Richter <Richterrettich@gmail.com>
2016-01-06 17:00:45 +0100

    Progress

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ go test
 go install
 cd ..
 go test
-GO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o out/main .
+CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o out/main .
 docker build -t openservice/media-service:latest .
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_REPO_SLUG}" = "InteractiveLecture/media-service" ] ; then
   docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD" -e="$DOCKER_EMAIL"
